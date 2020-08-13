316bc6a437473835ca89ea17c8be6f028cf9e1f0
ivan819 <ivan.angelovski@students.finki.ukim.mk>
2020-08-14 01:14:23 +0200

    spring boot app empty

---
+++ src/main/java/com/ivan/MoviesRDF/MoviesRdfApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.MoviesRDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/MoviesRDF/MoviesRdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.MoviesRDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
