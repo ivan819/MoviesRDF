c89b21682a5eae7564bc6c49462048fde5df2905
ivan819 <ivan.angelovski@students.finki.ukim.mk>
2020-09-11 19:25:31 +0200

    podelen controler i dodadeni novi htmls

--- src/main/java/com/ivan/MoviesRDF/rest/MainController.java
+++ src/main/java/com/ivan/MoviesRDF/rest/MainController.java
@@ -5,15 +5,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import com.ivan.MoviesRDF.enitity.CastMember;
-import com.ivan.MoviesRDF.enitity.Company;
-import com.ivan.MoviesRDF.enitity.CrewMember;
 import com.ivan.MoviesRDF.enitity.Genre;
-import com.ivan.MoviesRDF.enitity.Member;
 import com.ivan.MoviesRDF.service.JenaService;
 
 import org.apache.commons.io.IOUtils;
@@ -24,7 +18,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -33,41 +26,6 @@ public class MainController {
 
     @Autowired
     JenaService jenaService;
-
-    @GetMapping(value = "/genres1")
-    @ResponseBody
-    public List<Genre> getGenres(@RequestParam(required = false) Long movieId) {
-        if (movieId == null)
-            return jenaService.getGenreList();
-        else
-            return jenaService.getGenreList(movieId);
-    }
-
-    @GetMapping(value = "/companies")
-    @ResponseBody
-    public List<Company> getCompanies() {
-        return jenaService.getCompanyList().stream().sorted(Comparator.comparing(Company::getRevenue).reversed())
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping(value = "/member")
-    @ResponseBody
-    public Member getMember(@RequestParam Long memberId) {
-        return jenaService.getMember(memberId);
-    }
-
-    @GetMapping(value = "/cast")
-    @ResponseBody
-    public List<CastMember> getCast(@RequestParam Long movieId) {
-        return jenaService.getCastMembers(movieId).stream().sorted(Comparator.comparing(CastMember::getOrder))
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping(value = "/crew")
-    @ResponseBody
-    public List<CrewMember> getCrew(@RequestParam Long movieId) {
-        return jenaService.getCrewMembers(movieId).stream().collect(Collectors.toList());
-    }
 
     @GetMapping(value = "/home")
     public String home() {
@@ -79,6 +37,18 @@ public class MainController {
     public String movies() {
 
         return "app";
+    }
+
+    @GetMapping(value = "/productions")
+    public String productions() {
+
+        return "index";
+    }
+
+    @GetMapping(value = "/about")
+    public String about() {
+
+        return "about";
     }
 
     @GetMapping(value = "/genres")
